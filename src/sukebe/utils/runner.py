4d04d8b6cf7191bbd33efcccf1580cd63fd08c25
dia ♡ <60399731+6days9weeks@users.noreply.github.com>
2023-06-02 00:38:19 +0000

    rearrange

--- src/sukebe/utils/runner.py
+++ src/sukebe/utils/runner.py
@@ -38,70 +38,90 @@ def send_webhook(webhook: str, type_: str, name: str, file: BytesIO, client: HTT
     )
 
 
+def loop_reddit(webhooks: List[str], http: HTTPClient) -> None:
+    for subreddit in subreddits:
+        logger.info(f"Getting image from {subreddit}...")
+        image = http.get_reddit(subreddit)
+        if not image:
+            logger.error(f"Failed to get image from {subreddit}")
+            continue
+        logger.info(f"Sending image from {subreddit}...")
+        file = BytesIO(http.session.get(image).content)
+        for webhook in webhooks:
+            send_webhook(webhook, "reddit", subreddit, file, http)
+            time.sleep(0.5)
+        time.sleep(5)
+
+
+def loop_yandere(webhooks: List[str], http: HTTPClient) -> None:
+    for tag in yandere_tags:
+        logger.info(f"Getting image from yandere with tag {tag}...")
+        image = http.get_yandere(tag)
+        if not image:
+            logger.error(f"Failed to get image from yandere with tag {tag}")
+            continue
+        logger.info(f"Sending image from yandere with tag {tag}...")
+        file = BytesIO(http.session.get(image).content)
+        for webhook in webhooks:
+            send_webhook(webhook, "yandere", tag, file, http)
+            time.sleep(0.5)
+        time.sleep(5)
+
+
+def loop_nekoslife(webhooks: List[str], http: HTTPClient) -> None:
+    for tag in nekoslife_tags:
+        logger.info(f"Getting image from nekos.life with tag {tag}...")
+        image = http.get_nekos(tag)
+        if not image:
+            logger.error(f"Failed to get image from nekos.life with tag {tag}")
+            continue
+        logger.info(f"Sending image from nekos.life with tag {tag}...")
+        file = BytesIO(http.session.get(image).content)
+        for webhook in webhooks:
+            send_webhook(webhook, "nekos.life", tag, file, http)
+            time.sleep(0.5)
+        time.sleep(5)
+
+
+def loop_danbooru(webhooks: List[str], http: HTTPClient) -> None:
+    for tag in danbooru_tags:
+        logger.info(f"Getting image from danbooru with tag {tag}...")
+        image = http.get_danbooru(tag)
+        if not image:
+            logger.error(f"Failed to get image from danbooru with tag {tag}")
+            continue
+        logger.info(f"Sending image from danbooru with tag {tag}...")
+        file = BytesIO(http.session.get(image).content)
+        for webhook in webhooks:
+            send_webhook(webhook, "danbooru", tag, file, http)
+            time.sleep(0.5)
+        time.sleep(5)
+
+
+def loop_konachan(webhooks: List[str], http: HTTPClient) -> None:
+    for tag in konachan_tags:
+        logger.info(f"Getting image from konachan with tag {tag}...")
+        image = http.get_konachan(tag)
+        if not image:
+            logger.error(f"Failed to get image from konachan with tag {tag}")
+            continue
+        logger.info(f"Sending image from konachan with tag {tag}...")
+        file = BytesIO(http.session.get(image).content)
+        for webhook in webhooks:
+            send_webhook(webhook, "konachan", tag, file, http)
+            time.sleep(0.5)
+        time.sleep(5)
+
+
 def loop(webhooks: List[str], http: HTTPClient) -> None:
     logger.info("Starting loop...")
     while True:
         with suppress(Exception):
             logger.info("Iterating...")
-            for tag in yandere_tags:
-                logger.info(f"Getting image from yandere with tag {tag}...")
-                image = http.get_yandere(tag)
-                if not image:
-                    logger.error(f"Failed to get image from yandere with tag {tag}")
-                    continue
-                logger.info(f"Sending image from yandere with tag {tag}...")
-                file = BytesIO(http.session.get(image).content)
-                for webhook in webhooks:
-                    send_webhook(webhook, "yandere", tag, file, http)
-                    time.sleep(0.5)
-                time.sleep(5)
-            for subreddit in subreddits:
-                logger.info(f"Getting image from {subreddit}...")
-                image = http.get_reddit(subreddit)
-                if not image:
-                    logger.error(f"Failed to get image from {subreddit}")
-                    continue
-                logger.info(f"Sending image from {subreddit}...")
-                file = BytesIO(http.session.get(image).content)
-                for webhook in webhooks:
-                    send_webhook(webhook, "reddit", subreddit, file, http)
-                    time.sleep(0.5)
-                time.sleep(5)
-            for tag in danbooru_tags:
-                logger.info(f"Getting image from danbooru with tag {tag}...")
-                image = http.get_danbooru(tag)
-                if not image:
-                    logger.error(f"Failed to get image from danbooru with tag {tag}")
-                    continue
-                logger.info(f"Sending image from danbooru with tag {tag}...")
-                file = BytesIO(http.session.get(image).content)
-                for webhook in webhooks:
-                    send_webhook(webhook, "danbooru", tag, file, http)
-                    time.sleep(0.5)
-                time.sleep(5)
-            for tag in konachan_tags:
-                logger.info(f"Getting image from konachan with tag {tag}...")
-                image = http.get_konachan(tag)
-                if not image:
-                    logger.error(f"Failed to get image from konachan with tag {tag}")
-                    continue
-                logger.info(f"Sending image from konachan with tag {tag}...")
-                file = BytesIO(http.session.get(image).content)
-                for webhook in webhooks:
-                    send_webhook(webhook, "konachan", tag, file, http)
-                    time.sleep(0.5)
-                time.sleep(5)
-            for tag in nekoslife_tags:
-                logger.info(f"Getting image from nekos.life with tag {tag}...")
-                image = http.get_nekos(tag)
-                if not image:
-                    logger.error(f"Failed to get image from nekos.life with tag {tag}")
-                    continue
-                logger.info(f"Sending image from nekos.life with tag {tag}...")
-                file = BytesIO(http.session.get(image).content)
-                for webhook in webhooks:
-                    send_webhook(webhook, "nekos.life", tag, file, http)
-                    time.sleep(0.5)
-                time.sleep(5)
+            loop_reddit(webhooks, http)
+            loop_yandere(webhooks, http)
+            loop_nekoslife(webhooks, http)
+            loop_danbooru(webhooks, http)
+            loop_konachan(webhooks, http)
             logger.info("Finished iterating, sleeping for 30 minutes...")
             time.sleep(1800)
