8f6549b5ac0c43a0d6c9fbf161343c2c261d0916
dia ♡ <60399731+6days9weeks@users.noreply.github.com>
2023-06-02 12:03:01 +0000

    fix

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,24 @@
 # pylint: disable=missing-module-docstring
+import os
+from pathlib import Path
+import sys
+
 from loguru import logger  # pylint: disable=import-error
 
 from sukebe import HTTPClient, load_webhooks, loop
 
 if __name__ == "__main__":
     logger.info("Starting sukebe...")
+    if not os.path.exists(Path.cwd() / "config.json"):
+        logger.error("config.json not found!")
+        sys.exit(1)
+    if not os.path.exists(Path.cwd() / "webhooks.txt"):
+        logger.error("webhooks.txt not found!")
+        sys.exit(1)
     hooks = load_webhooks()
     session = HTTPClient()
     try:
         loop(hooks, session)
     except KeyboardInterrupt:
         logger.info("Exiting...")
-        exit(0)
+        sys.exit(0)
